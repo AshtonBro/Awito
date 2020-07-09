99a0c6aba5662827885728a96298433d501f6860
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-09 09:28:14 +0500

    this === modalAdd

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
-'use strict'
+"use strict";
 
-const modalAdd = document.querySelector('.modal__add'),
-    btnAddAd = document.querySelector('.add__ad'),
-    modalClose = document.querySelector('.modal__close'),
-    header = document.querySelector('.header');
+const modalAdd = document.querySelector(".modal__add"),
+  btnAddAd = document.querySelector(".add__ad"),
+  btnModalSubmin = document.querySelector(".modal__btn-submit"),
+  modalSubmit = document.querySelector(".modal__submit"),
+  catalog = document.querySelector(".catalog"),
+  modalItem = document.querySelector(".modal__item");
 
-btnAddAd.addEventListener('click', () => {
-    modalAdd.classList.toggle('hide');
-});
+const closeModal = function (event) {
+  const target = event.target;
 
-modalAdd.addEventListener('click', (event) => {
-    var target = event.target;
-    if (target.matches('.modal__close') || target == modalAdd) {
-        modalAdd.classList.toggle('hide');
+  if (target.closest(".modal__close") || target === this) {
+    this.classList.add("hide");
+    if (this === modalAdd) {
+      modalSubmit.reset();
     }
+  }
+};
+
+btnAddAd.addEventListener("click", () => {
+  modalAdd.classList.remove("hide");
+  btnModalSubmin.disabled = true;
 });
+
+modalAdd.addEventListener("click", closeModal);
+modalItem.addEventListener("click", closeModal);
+
+catalog.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.closest('.card')) {
+    modalItem.classList.remove('hide');
+  }
+});
+
+document.querySelector('keydown', event => {
+
+})
